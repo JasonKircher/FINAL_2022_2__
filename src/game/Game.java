859a06805dde1dca0a2920386f8e5f15c869705c
Jason Kircher <ljkircher@gmail.com>
2022-03-01 12:59:38 +0100

    init

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,19 +1,28 @@
 package game;
 
+import game.gameParts.cards.abilities.AbilityCard;
+import game.gameParts.cards.monsters.MonsterCard;
 import game.gameParts.player.Rauna;
 import game.state.GameState;
 import game.state.InitSetUp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Game {
-    private GameState state;
-    private int level;
-    private final Rauna rauna;
+    private         GameState           state;
+    private         int                 level;
+    private final   Rauna               rauna;
+    private final   List<AbilityCard>   abilityCards;
+    private final   List<MonsterCard>   monsterCards;
 
 
     public Game() {
         this.level = 1;
         this.rauna = new Rauna();
         this.state = new InitSetUp(this);
+        this.abilityCards = new LinkedList<>();
+        this.monsterCards = new LinkedList<>();
     }
 
     public void start() {
--- src/game/gameParts/player/Rauna.java
+++ src/game/gameParts/player/Rauna.java
@@ -1,10 +1,30 @@
 package game.gameParts.player;
 
+import game.gameParts.cards.abilities.AbilityCard;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Rauna {
-    private int hp;
-    private int abilityLevel;
+    private int                 hp;
+    private int                 abilityLevel;
+    private List<AbilityCard>   abilityCards;
+
     public Rauna() {
+        this.abilityCards = new LinkedList<>();
         this.hp = PlayerStartingValues.STARTING_HP.getValue();
         this.abilityLevel = PlayerStartingValues.STARTING_LEVEL.getValue();
+    }
+
+    public boolean takeDamage(int damage) {
+        this.hp -= damage;
+        return hp >= 0;
+    }
+
+    public void increaseLevel() {
+        abilityLevel++;
+    }
+    public void addCard(AbilityCard card) {
+        abilityCards.add(card);
     }
 }
