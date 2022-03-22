f050cc19dad92ab6585ca65be1bc2ae3bf8ad7dd
Jason Kircher <ljkircher@gmail.com>
2022-03-22 16:31:47 +0100

    handed in

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,11 +1,14 @@
 package game;
 
+import java.util.Arrays;
+
 /**
  * main class to start the game
  * @author upvlx
  * @version 0.1
  */
 public final class Main {
+    private static final String ERROR_MSG = "Error, this game is supposed to run without parameters";
 
     /**
      * private constructor for execution class
@@ -17,9 +20,11 @@ public final class Main {
      * @param args commandline arguments
      */
     public static void main(String[] args) {
-
-        RunasStrive runasStrive = new RunasStrive();
-        runasStrive.start();
-
+        if (!Arrays.stream(args).toList().isEmpty())
+            System.out.println(ERROR_MSG);
+        else {
+            RunasStrive runasStrive = new RunasStrive();
+            runasStrive.start();
+        }
     }
 }
